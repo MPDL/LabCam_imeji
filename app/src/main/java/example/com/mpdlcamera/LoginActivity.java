30b6a39e34753b2ccaa05ed14c02d0abd0ece04a
shabnam <kiranj91@github.com>
2015-09-07 11:38:42 +0200

    testing again and again

--- app/src/main/java/example/com/mpdlcamera/LoginActivity.java
+++ app/src/main/java/example/com/mpdlcamera/LoginActivity.java
@@ -57,7 +57,7 @@ public class LoginActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        //testing again
+        //testing again and again
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
