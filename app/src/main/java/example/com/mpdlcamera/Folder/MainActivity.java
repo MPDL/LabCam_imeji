026036629a809437f2af480aa2bd2897f86cbbd8
Allen Shi <shidenghui@gmail.com>
2015-11-03 16:40:58 +0100

    hidde the settings dots

--- app/src/main/java/example/com/mpdlcamera/Folder/MainActivity.java
+++ app/src/main/java/example/com/mpdlcamera/Folder/MainActivity.java
@@ -352,7 +352,7 @@ public class MainActivity extends AppCompatActivity implements UploadResultRecei
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
