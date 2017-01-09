2ac429d6251733a6e37343b894d72fecb2cd9f61
pfingsta <work.soultice@gmail.com>
2017-01-09 14:29:02 +0100

    Fixed an issue where sometimes the text would appear scrambled in the final result

---
+++ app/src/main/java/de/mpg/mpdl/labcam/Objects/LineAttributes.java
@@ -0,0 +1,57 @@
+package de.mpg.mpdl.labcam.Objects;
+
+/**
+ * Created by pfingsta on 09.01.2017.
+ */
+
+public class LineAttributes {
+    public float left;
+    public float top;
+    public float right;
+    public float bottom;
+    public String text;
+    public float height;
+    public boolean headline;
+
+    public LineAttributes(float l, float t, float r, float b, String txt, float h, boolean hdl) {
+        left = l;
+        top = t;
+        right = r;
+        bottom = b;
+        text = txt;
+        height = h;
+        headline = hdl;
+    }
+
+    public float getTop() {
+        return this.top;
+    }
+
+    public float getLeft() {
+        return this.left;
+    }
+
+    public float getRight() {
+        return this.right;
+    }
+
+    public float getBottom() {
+        return this.bottom;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public float getHeight() {
+        return this.height;
+    }
+
+    public boolean isHeadline(){
+        return this.headline;
+    }
+
+    public void setIsHeadline(Boolean hl){
+        this.headline = hl;
+    }
+}
--- app/src/main/java/de/mpg/mpdl/labcam/Utils/OCRtextHandler.java
+++ app/src/main/java/de/mpg/mpdl/labcam/Utils/OCRtextHandler.java
@@ -2,19 +2,26 @@ package de.mpg.mpdl.labcam.Utils;
 
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.graphics.Bitmap;
-import android.util.Log;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
 import android.util.SparseArray;
 
 import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.MultiDetector;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.android.gms.vision.text.Line;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-import static android.content.ContentValues.TAG;
+import de.mpg.mpdl.labcam.Objects.LineAttributes;
 
 /**
  * Created by soultice on 02-Oct-16.
@@ -23,44 +30,61 @@ import static android.content.ContentValues.TAG;
 public class OCRtextHandler {
 
     public static String getText(Context innercontext, Bitmap bitmap) {
+
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+        Bitmap compressed = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
+
         Frame newFrame = new Frame.Builder()
-                .setBitmap(bitmap)
+                .setBitmap(compressed)
                 .build();
+
         //initialize Textrecognizer
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(innercontext).build();
         TextRecognizer textRecognizer = new TextRecognizer.Builder(innercontext).build();
+
+
         //parse text
-        SparseArray<TextBlock> currentText = textRecognizer.detect(newFrame);
+        MultiDetector multiDetector = new MultiDetector.Builder()
+                .add(barcodeDetector)
+                .add(textRecognizer)
+                .build();
 
-        if (!textRecognizer.isOperational()) {
-            Log.e(TAG, "Detector dependencies are not yet available.");
+        SparseArray<Object> objectSparseArray = multiDetector.detect(newFrame);
 
-            // Check for low storage.  If there is low storage, the native library will not be
-            // downloaded, so detection will not become operational.
-            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-//            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
-//
-//            if (hasLowStorage) {
-//                Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
-//                Log.w(TAG, getString(R.string.low_storage_error));
-        }
-        //Add text to an arraylist
-        ArrayList<String> fullText = new ArrayList<String>();
-        if (currentText.size() == 0) {
-            fullText.add("No text found");
-        }
-        for (int i = 0; i < currentText.size(); ++i) {
-            TextBlock item = currentText.valueAt(i);
-            if (item != null && item.getValue() != null) {
-                fullText.add(item.getValue());
+        ArrayList<LineAttributes> lineAttributes = new ArrayList<>();
+
+        for (int i = 0; i < objectSparseArray.size(); i++) {
+            Object currObject = objectSparseArray.valueAt(i);
+            if (currObject instanceof TextBlock) {
+                TextBlock item = (TextBlock) currObject;
+                List<Line> lines = (List<Line>) ((TextBlock) currObject).getComponents();
+                for(Line elements: lines){
+                    RectF rect = new RectF(elements.getBoundingBox());
+                    float height = rect.bottom - rect.top;
+                    LineAttributes attr = new LineAttributes(rect.left, rect.top, rect.right, rect.bottom, elements.getValue(), height, false);
+                    lineAttributes.add(attr);
+                }
             }
         }
-        //Convert arraylist to stringbuilder
+        multiDetector.release();
+        textRecognizer.release();
+        barcodeDetector.release();
+
+
+        Collections.sort(lineAttributes, new Comparator<LineAttributes>() {
+            @Override
+            public int compare(LineAttributes o1, LineAttributes o2) {
+                return (Float.compare(o1.getTop(), o2.getTop()));
+            }
+        });
+
         StringBuilder sb = new StringBuilder();
-        for (String s : fullText) {
-            sb.append(s);
-            sb.append("\t");
+        for (LineAttributes line : lineAttributes) {
+            sb.append(line.getText());
+            sb.append("\n");
         }
-        String message = sb.toString();
-        return message;
+        return (sb.toString());
     }
 }
