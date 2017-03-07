50400456ac0f067d61d3d9b77745d9c2352862d3
Ying <paipaibear90@gmail.com>
2017-03-07 10:30:40 +0100

    Add PrefernceUtil for managing sharedPreference

---
+++ app/src/main/java/de/mpg/mpdl/labcam/code/utils/Constants.java
@@ -0,0 +1,17 @@
+package de.mpg.mpdl.labcam.code.utils;
+
+/**
+ * Created by yingli on 3/7/17.
+ */
+
+public class Constants {
+
+    /**************************   SHARED_PREFERENCES **********************************/
+    public static final String SHARED_PREFERENCES = "myPref"; // name of shared preferences
+
+    public static final String API_KEY = "apiKey";
+    public static final String USER_ID = "userId";
+    public static final String SERVER = "server";
+
+
+}
